a6ea82c9f25a7660ea534f503c19c99a96a513bf
kparks29 <ken@awesome>
2014-08-19 14:50:31 -0700

    As a user when I come to the page I want to be able to see a list of items

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Item.create([{name: 'Orange', price: 0.50, description: "Juicy!!!", image_url: "http://realfood.tesco.com/media/images/Orange-and-almond-srping-cake-hero-58d07750-0952-47eb-bc41-a1ef9b81c01a-0-472x310.jpg"},{name: 'Avocado', price: 2.00, description: "Creamy!", image_url: "http://mikes-table.themulligans.org/wp-content/uploads/2008/08/creamy_avocado_soup-1.jpg"}])
